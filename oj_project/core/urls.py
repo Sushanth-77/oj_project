e6486809c9cdcc5fec3babfe43d85c7d6d24dc26
Sushanth Reddy <sushanthreddychanti.com>
2025-07-14 18:08:32 +0530

    Updated some code and tried running the auth part

--- oj_project/core/urls.py
+++ oj_project/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path , include
 from .views import register_user, login_user, logout_user
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path('register/',register_user,name='register-user'),
+    path('register/', register_user, name='register-user'),
     path('login/',login_user,name='login-user'),
     path('logout/',logout_user,name='logout-user'),
 ]
