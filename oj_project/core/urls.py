0e3100d772dc4165c9f86012482bdf32633a59e9
Sushanth Reddy <sushanthreddychanti.com>
2025-07-20 15:08:03 +0530

    test 3

--- oj_project/core/urls.py
+++ oj_project/core/urls.py
@@ -1,4 +1,3 @@
-# oj_project/core/urls.py
 from django.urls import path
 from .views import (
     register_user, login_user, logout_user, 
@@ -8,10 +7,11 @@ from .views import (
 app_name = 'core'
 
 urlpatterns = [
+    path('', lambda request: redirect('/problems/', permanent=False)),
     path('register/', register_user, name='register-user'),
     path('login/', login_user, name='login-user'),
     path('logout/', logout_user, name='logout-user'),
     path('problems/', problems_list, name='problems_list'),
-    path('problem/<str:short_code>/', problem_detail, name='problem_detail'),
+    path('problems/<str:short_code>/', problem_detail, name='problem_detail'),
     path('submissions/', submissions_list, name='submissions_list'),
 ]
