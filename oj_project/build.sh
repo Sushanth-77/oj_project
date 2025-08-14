b07c7204d8f85fabb7b1a4e1d321ee4c6136b2cb
Sushanth Reddy <sushanthreddychanti.com>
2025-08-15 01:21:05 +0530

    test41

--- oj_project/build.sh
+++ oj_project/build.sh
@@ -2,11 +2,17 @@
 # exit on error
 set -o errexit
 
+# Make sure we have proper permissions (Linux/Unix)
+chmod +x "$0" 2>/dev/null || true
+
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
-python manage.py collectstatic --no-input
+# Create migrations (in case any are missing)
+python manage.py makemigrations
 
 # Run migrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --no-input
